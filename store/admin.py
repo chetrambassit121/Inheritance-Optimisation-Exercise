90964dacb687d9853fe77dc44170f41b7d1e427d
Chetram Bassit <chetrambassit121@gmail.com>
2022-07-06 23:27:51 -0400

    set up admin for store models

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, Book, Cupboard
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Book)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Cupboard)
+class ProductAdmin(admin.ModelAdmin):
+    pass
